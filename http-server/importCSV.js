30c81f2f6ba76e8be601809133dad902459b6b22
leo <wanglingsong@gmail.com>
2017-04-21 15:27:38 +0800

    Improve csv importer

--- http-server/importCSV.js
+++ http-server/importCSV.js
@@ -7,29 +7,39 @@ var request = require('request');
 
 var csv = require('csv-parser');
 
+function uploadData(metric, sensorId, data, dataField) {
+    var sensorData = {
+        metricName: metric,
+        tags: {
+            sensorId: sensorId,
+            organization: "UCI"
+        },
+        timestamp: [moment(data.Date + ' ' + data.Time, "DD/MM/YYYY HH.mm.ss").valueOf()],
+        doubles: [parseFloat(data[dataField].replace(',', '.'))]
+    };
+
+    //console.log(sensorData);
+    request.post({
+        url: 'http://localhost:8081/sensor',
+        json: true,
+        body: sensorData
+    }, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        //console.info('Upload successful!  Server responded with:', body);
+    });
+}
+
 fs.createReadStream(__dirname + '/AirQualityUCI.csv')
-    .pipe(csv({separator: ';'}))
+    .pipe(csv({
+        separator: ';'
+    }))
     .on('data', function (data) {
-        var sensorData = {
-            metricName: "CO",
-            tags: {
-                sensorId: "co-sensor",
-                organization: "UCI"
-            },
-            timestamp: [moment(data.Date + ' ' + data.Time, "DD/MM/YYYY HH.mm.ss").valueOf()],
-            doubles: [parseFloat(data['CO(GT)'].replace(',', '.'))]
-        };
+        //uploadData('CO', 'sensor1', data, 'CO(GT)');
+        //uploadData('NMHC', 'sensor2', data, 'NMHC(GT)');
+        //uploadData('C6H6', 'sensor3', data, 'C6H6(GT)');
+        uploadData('NOx', 'sensor4', data, 'NOx(GT)');
 
-        //console.log(sensorData);
-        request.post({
-            url: 'http://localhost:8081/sensor',
-            json: true,
-            body: sensorData
-        }, function optionalCallback(err, httpResponse, body) {
-            if (err) {
-                return console.error('upload failed:', err);
-            }
-            console.info('Upload successful!  Server responded with:', body);
-        });
-
+        
     });
